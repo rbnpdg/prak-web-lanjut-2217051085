4af432e6877527370810064b728278ac6a6d1436
Ahmad Kassim <112568840+rbnpdg@users.noreply.github.com>
2024-10-21 11:13:00 +0700

    Update File

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -11,17 +11,12 @@ class UserModel extends Model
 
     protected $table = 'user';
     protected $guarded = ['id'];
+    protected $fillable = ['nama', 'npm', 'kelas_id', 'foto'];
 
     public function kelas() {
         return $this->belongsTo(Kelas::class, 'kelas_id');
     }
 
-<<<<<<< Updated upstream
-    public function getUser() {            
-        return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')->select('user.*', 'kelas.nama_kelas as nama_kelas')->get();
-    }
-}
-=======
     public function getUser($id = null) {
         if ($id != null) {
             return $this->join('kelas', 'user.kelas_id', '=', 'kelas.id')
@@ -37,4 +32,3 @@ class UserModel extends Model
 
     
 }
->>>>>>> Stashed changes
